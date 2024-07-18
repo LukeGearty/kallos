f2a97d37891c2fa9328e1f8462ed87a2405d1b92
LukeGearty <lukegearty@gmail.com>
2024-07-18 08:38:37 -0400

    started a placeholder /stretch/main.py

---
+++ stretch/main.py
@@ -0,0 +1,11 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from format_date import format_date
+
+def main():
+    print("You can barely touch your toes! Start stretching now!")
+
+if __name__ == "__main__":
+    main()
