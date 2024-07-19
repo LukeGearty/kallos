89922bb6d34881f8986ffeebff820d14e277f38b
LukeGearty <lukegearty@gmail.com>
2024-07-19 11:30:30 -0400

    added the first step in the stretching portion

--- stretch/inflexbile.py
+++ stretch/inflexbile.py
@@ -1,4 +1,5 @@
 import random
 
-def beginner_stretch_upper():
-    
+def gentle_stretch():
+    stretches = ['Downward Facing Dog', 'Cat Cow', 'Standing Calf Raise', 'Shoulder Rolls', 'Shoulder Reaches']
+    return random.choice(stretches)
--- stretch/main.py
+++ stretch/main.py
@@ -3,10 +3,37 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from format_date import format_date
+import inflexbile as inflx
+
+
+def gentle_stretching():
+    for i in range(1, 4):
+        print(inflx.gentle_stretch())
+    print("Hold each pose for 30 seconds")
+
 
 def main():
     date = format_date()
-    print(date)
+    print(f"Welcome to the stretching session of {date}")
+    print("Do you want this stretching to be (1) Gentle, (2) Challenging, (3) Intense?")
+    choice = 0
+    while choice not in [1, 2, 3]:
+        try:
+            choice = int(input())
+            if choice not in [1, 2, 3]:
+                print("Please select one of the following options: ")
+                print("1. Gentle")
+                print("2. Challenging")
+                print("3. Intense")
+                choice = int(input())
+            else:
+                break
+        except ValueError:
+            print("Please Enter an integer as your selection")
+    
+    if choice == 1:
+        gentle_stretching()
+
 
 if __name__ == "__main__":
     main()
