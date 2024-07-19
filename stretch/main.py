342a3525a69b39f6c194fcdf846a20e99600e192
LukeGearty <lukegearty@gmail.com>
2024-07-19 11:11:35 -0400

    started work on beginners for stretching

--- stretch/main.py
+++ stretch/main.py
@@ -5,7 +5,8 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from format_date import format_date
 
 def main():
-    print("You can barely touch your toes! Start stretching now!")
+    date = format_date()
+    print(date)
 
 if __name__ == "__main__":
     main()
