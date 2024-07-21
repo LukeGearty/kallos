46b1e39092ec1d06410b8cbef892455013c9b3b0
LukeGearty <lukegearty@gmail.com>
2024-07-21 10:13:51 -0400

    created the function in challenging_stretch.py

--- stretch/challenging_stretch.py
+++ stretch/challenging_stretch.py
@@ -1,1 +1,6 @@
 import random
+
+
+def challenging():
+    stretches = ['Standing Forward Fold', 'Lying Forward Fold','Deep Squat', 'Deep Lunges', 'Bridge', 'Supine Spinal Twists', 'Pigeon Pose']
+    return random.choice(stretches)
